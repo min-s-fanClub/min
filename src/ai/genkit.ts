b2db5462fdf61eb33c989c10d42b7bac60fecd47
sehuiiii <parksehui.dev@gmail.com>
2025-07-16 11:46:40 +0900

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,11 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import { genkit } from 'genkit';
+import { googleAI } from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: '', //API 직접 삽입!
+    }),
+  ],
   model: 'googleai/gemini-2.5-flash',
 });
